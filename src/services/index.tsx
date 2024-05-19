2b6298140469eba0aee287febf4523ed2cda84fb
Joy Ivan <ivanoskytan@gmail.com>
2024-05-19 23:39:58 +0700

    feat: add services

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -12,3 +12,96 @@ export function createSchedule(payload: any) {
     })
 } 
 
+export function updateSchedule(payload: any, id: number) {
+    return fetch(`${API_SLAITEIO}/api/schedule/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function deleteSchedule(id: number) {
+    return fetch(`${API_SLAITEIO}/api/schedule/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+export function getSchedulesByUser(user_id: number) {
+    return fetch(`${API_SLAITEIO}/api/schedule/user/${user_id}`, {
+        method: 'GET'
+    })
+}
+
+export function createEvent(payload: any) {
+    return fetch(`${API_SLAITEIO}/api/event`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function createTask(payload: any) {
+    return fetch(`${API_SLAITEIO}/api/task`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function updateEvent(payload: any, event_id: number) {
+    return fetch(`${API_SLAITEIO}/api/event/${event_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function updateTask(payload: any, task_id: number) {
+    return fetch(`${API_SLAITEIO}/api/task/${task_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function deleteActivity(activity_id: number) {
+    return fetch(`${API_SLAITEIO}/api/activity/${activity_id}`, {
+        method: 'DELETE',
+    })
+}
+
+export function createNotification(payload: any) {
+    return fetch(`${API_SLAITEIO}/api/notification`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function updateNotification(payload: any, notification_id: number) {
+    return fetch(`${API_SLAITEIO}/api/notification/${notification_id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(payload)
+    })
+}
+
+export function getNotificationsByActivity(activity_id: number){
+    return fetch(`${API_SLAITEIO}/api/notification/${activity_id}`, {
+        method: 'GET'
+    })
+}
