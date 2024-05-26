21e8c02a825320ebf6967e1969ff3137512a1e65
Joy Ivan <ivanoskytan@gmail.com>
2024-05-27 03:18:30 +0700

    feat: display all schedules in schedules main page

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -4,12 +4,13 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"
 import SchedulePanel from '@/components/SchedulePanel';
 import { PiPencilSimpleLineFill } from "react-icons/pi";
-import { FaWandMagicSparkles } from "react-icons/fa6";
+import { FaNoteSticky, FaWandMagicSparkles } from "react-icons/fa6";
 import { useEffect, useState } from 'react';
 import useAuthStore from '@/store/authStore';
 import Panel from '@/components/Panel';
 import { getSchedulesByUser } from '@/services';
 import { useRouter } from 'next/router';
+import { FaTasks } from 'react-icons/fa';
 
 export default function Schedules() {
 
@@ -21,16 +22,44 @@ export default function Schedules() {
     const response = await getSchedulesByUser(String(user?.email), undefined, true);
     const data = await response.json();
     setSchedules(data?.data);
+    console.log(schedules)
   }
 
   useEffect(() => {
     getData();
   }, []);
 
+  const renderEventContent = (eventInfo: any) => {
+    const type = eventInfo.event.extendedProps.type;
+
+    if (type === 'TASK') {
+      return (
+        <div className="flex flex-col gap-1 px-2 py-1">
+          <div className="flex items-center hover:opacity-90 font-bold">
+            <FaTasks />
+            <span className="ml-2">{eventInfo.event.title}</span>
+          </div>
+          <span className='font-bold'>{eventInfo.event.extendedProps.metadata?.due_time}</span>
+        </div>
+      )
+    }
+    else if (type === 'EVENT') {
+      return (
+        <div className='flex flex-col gap-1 px-2 py-1'>
+          <div className="flex items-center hover:opacity-90 font-bold">
+            <FaNoteSticky />
+            <span className="ml-2">{eventInfo.event.title}</span>
+          </div>
+          <span className='font-bold'>{eventInfo.event.extendedProps.metadata?.start_time} - {eventInfo.event.extendedProps.metadata?.end_time}</span>
+        </div>
+      )
+    }
+  };
+
   return (
     <div className="min-h-screen flex relative">
-      <Panel />
-      <div className="fixed bottom-0 right-0 mb-12 mr-12 flex gap-5">
+        <Panel />
+      <div className="fixed bottom-0 right-0 mb-12 mr-12 flex gap-5 z-10">
         <button className='bg-[#008080] rounded-xl shadow-xl p-5 cursor-pointer hover:scale-105 hover:opacity-90' onClick={() => router.push('/schedule/create')}>
           <PiPencilSimpleLineFill className='w-20 h-20 text-white cursor-pointer'/>
         </button>
@@ -38,13 +67,23 @@ export default function Schedules() {
           <FaWandMagicSparkles className='w-20 h-20 text-white cursor-pointer'/>
         </button>
       </div>
-      <div className="flex-grow max-h-[100vh] overflow-auto px-10 py-5">
-        <h1 className='mt-5 mb-10 text-4xl font-bold'>All Schedules</h1>
-        {/* <FullCalendar 
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-
-        /> */}
+      <div className="flex-grow max-h-[100vh] overflow-auto px-10 py-20">
+        <h1 className='text-3xl font-bold mb-5'>All schedules</h1>
+        {schedules && (
+          <>
+            <FullCalendar 
+              plugins={[dayGridPlugin]}
+              initialView="dayGridMonth"
+              events={schedules?.flatMap((schedule: any) => schedule.activities.map((activity: any) => ({
+                title: activity.type === 'TASK' ? activity.metadata.name : activity.metadata.title,
+                date: activity.date,
+                type: activity.type,
+                metadata: activity.metadata
+              })))}
+              eventContent={renderEventContent} 
+            />
+          </>
+        )}
       </div>
     </div>
   )
