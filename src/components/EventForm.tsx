ebf53469d2ce7c8ba1f75e3c05733c766f06474d
Joy Ivan <ivanoskytan@gmail.com>
2024-05-27 00:43:30 +0700

    feat: add notifications handler and form in event form

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
+import { IoIosNotifications } from 'react-icons/io';
 import 'tailwindcss/tailwind.css';
 
 interface EventFormProps {
     onAddActivity: (newEvent: any) => void
+}
+
+interface Notification {
+    message: string;
+    status: string;
+    time: string;
 }
 
 export default function EventForm({ onAddActivity }: EventFormProps) {
@@ -13,9 +20,11 @@ export default function EventForm({ onAddActivity }: EventFormProps) {
     const [endTime, setEndTime] = useState<string>("00:00");
     const [date, setDate] = useState<string>("");
     const [repeatInterval, setRepeatInterval] = useState<string>("");
+    const [openNotificationOptions, setOpenNotificationOptions] = useState<boolean>(false);
+    const [notifications, setNotifications] = useState<Notification[]>([]);
 
     const handleSave = () => {
-        const newEvent = {
+        const newEvent: any = {
             date: date,
             type: 'EVENT',
             repeat_interval: repeatInterval,
@@ -26,7 +35,75 @@ export default function EventForm({ onAddActivity }: EventFormProps) {
                 end_time: endTime
             }
         }
+
+        if (notifications.length > 0) {
+            newEvent.notifications = notifications;
+        }
+
         onAddActivity(newEvent);
+        setTitle("");
+        setDescription("");
+        setStartTime("");
+        setEndTime("");
+        setNotifications([]);
+    }
+
+    const handleAddNotifications = (notification: string) => {
+        const message = formatNotificationLabel(notification);
+        const time = calculateNotificationTime(notification);
+        const notificationPayload = {
+            message: message,
+            time: time,
+            status: 'HOLD'
+        }
+        setNotifications(prevNotifications => [...prevNotifications, notificationPayload]);
+    }
+
+    const formatNotificationLabel = (input: string): string => {
+        const timeMatch = input.match(/(\d+)([dhm])/);
+    
+        if (timeMatch) {
+            const value = parseInt(timeMatch[1]);
+            const unit = timeMatch[2];
+    
+            switch (unit) {
+                case 'h':
+                    return `${value} hour${value > 1 ? 's' : ''} before`;
+                case 'm':
+                    return `${value} minute${value > 1 ? 's' : ''} before`;
+                case 'd':
+                    return `${value} day${value > 1 ? 's' : ''} before`;
+                default:
+                    return '';
+            }
+        }
+    
+        return '';
+    }
+
+    const calculateNotificationTime = (value: string) => {
+        const notificationTime = new Date(`${date}T${startTime}`);
+
+        const daysMatch = value.match(/(\d+)d/);
+        const hoursMatch = value.match(/(\d+)h/);
+        const minutesMatch = value.match(/(\d+)m/);
+        
+        if (daysMatch) {
+            const days = parseInt(daysMatch[1]);
+            notificationTime.setDate(notificationTime.getDate() - days);
+        }
+
+        if (hoursMatch) {
+            const hours = parseInt(hoursMatch[1]);
+            notificationTime.setHours(notificationTime.getHours() - hours);
+        }
+
+        if (minutesMatch) {
+            const minutes = parseInt(minutesMatch[1]);
+            notificationTime.setMinutes(notificationTime.getMinutes() - minutes);
+        }
+
+        return notificationTime.toISOString();
     }
 
     return (
@@ -72,6 +149,68 @@ export default function EventForm({ onAddActivity }: EventFormProps) {
                     <option value="Weekly">Every week</option>
                     <option value="Monthly">Every month</option>
                 </select>
+                <div className="flex p-2 items-center bg-slate-100 cursor-pointer hover:bg-slate-200 rounded-md gap-2">
+                    <IoIosNotifications className='text-xl'/>
+                    <div className="flex flex-col justify-center">
+                        <div className="flex flex-col gap-4 p-2">
+                            {notifications?.map((notification) => (
+                                <div className="font-semibold">{notification.message}</div>
+                            ))}
+                        </div>
+                        <div 
+                            className="text-black font-semibold p-2 rounded-md hover:bg-slate-300 w-[100%]"
+                            onClick={() => setOpenNotificationOptions(!openNotificationOptions)}
+                        >
+                            Add Notification
+                        </div>
+                    </div>
+                </div>
+                {openNotificationOptions && (
+                    <div className="fixed top-0 left-3/4 bg-white p-4 shadow-md rounded-md w-[20%] mt-20 ml-20 z-50">
+                        <p className='font-semibold'>Notification Options</p>
+                        <div className="flex flex-col gap-2 mt-5">
+                            <option 
+                                value="5h" 
+                                className='p-3 font-semibold rounded-md bg-gray-200 cursor-pointer hover:bg-gray-300'
+                                onClick={() => handleAddNotifications("5h")}
+                            >
+                                5 hours before
+                            </option>
+                            <option 
+                                value="4h" 
+                                className='p-3 font-semibold rounded-md bg-gray-200 cursor-pointer hover:bg-gray-300'
+                                onClick={() => handleAddNotifications("4h")}
+                            >
+                                4 hours before
+                            </option>
+                            <option 
+                                value="3h" 
+                                className='p-3 font-semibold rounded-md bg-gray-200 cursor-pointer hover:bg-gray-300'
+                                onClick={() => handleAddNotifications("3h")}
+
+                            >
+                                3 hours before
+                            </option>
+                            <option 
+                                value="2h" 
+                                className='p-3 font-semibold rounded-md bg-gray-200 cursor-pointer hover:bg-gray-300'
+                                onClick={() => handleAddNotifications("2h")}
+
+                            >
+                                2 hours before
+                            </option>
+                            <option 
+                                value="30m" 
+                                className='p-3 font-semibold rounded-md bg-gray-200 cursor-pointer hover:bg-gray-300'
+                                onClick={() => handleAddNotifications("30m")}
+
+                            >
+                                30 minutes before
+                            </option>
+
+                        </div>
+                    </div>
+                )}
             </div>
             <div className='flex justify-between p-4'>
                 <button 
